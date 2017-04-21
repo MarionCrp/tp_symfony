f9f5c12fac6d216e45156d6ea1b91a14464d7209
MarionCrp <marion.craipeau@gmail.com>
2017-04-21 19:30:51 +0200

    Refacto vitrine views

--- src/sil16/VitrineBundle/Controller/ProductCategoryController.php
+++ src/sil16/VitrineBundle/Controller/ProductCategoryController.php
@@ -2,123 +2,14 @@
 
 namespace sil16\VitrineBundle\Controller;
 
-use sil16\VitrineBundle\Entity\ProductCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
-/**
- * Productcategory controller.
- *
- */
 class ProductCategoryController extends Controller
 {
-    /**
-     * Lists all productCategory entities.
-     *
-     */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $productCategories = $em->getRepository('sil16VitrineBundle:ProductCategory')->findAll();
-
-        return $this->render('productcategory/index.html.twig', array(
-            'productCategories' => $productCategories,
-        ));
-    }
-
-    /**
-     * Creates a new productCategory entity.
-     *
-     */
-    public function newAction(Request $request)
-    {
-        $productCategory = new Productcategory();
-        $form = $this->createForm('sil16\VitrineBundle\Form\ProductCategoryType', $productCategory);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($productCategory);
-            $em->flush($productCategory);
-
-            return $this->redirectToRoute('product_category_show', array('id' => $productCategory->getId()));
-        }
-
-        return $this->render('productcategory/new.html.twig', array(
-            'productCategory' => $productCategory,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a productCategory entity.
-     *
-     */
-    public function showAction(ProductCategory $productCategory)
-    {
-        $deleteForm = $this->createDeleteForm($productCategory);
-
-        return $this->render('productcategory/show.html.twig', array(
-            'productCategory' => $productCategory,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing productCategory entity.
-     *
-     */
-    public function editAction(Request $request, ProductCategory $productCategory)
-    {
-        $deleteForm = $this->createDeleteForm($productCategory);
-        $editForm = $this->createForm('sil16\VitrineBundle\Form\ProductCategoryType', $productCategory);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('product_category_edit', array('id' => $productCategory->getId()));
-        }
-
-        return $this->render('productcategory/edit.html.twig', array(
-            'productCategory' => $productCategory,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a productCategory entity.
-     *
-     */
-    public function deleteAction(Request $request, ProductCategory $productCategory)
-    {
-        $form = $this->createDeleteForm($productCategory);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($productCategory);
-            $em->flush($productCategory);
-        }
-
-        return $this->redirectToRoute('product_category_index');
-    }
-
-    /**
-     * Creates a form to delete a productCategory entity.
-     *
-     * @param ProductCategory $productCategory The productCategory entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(ProductCategory $productCategory)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('product_category_delete', array('id' => $productCategory->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
+      $em = $this->getDoctrine()->getManager();
+      $product_categories = $em->getRepository('sil16VitrineBundle:ProductCategory')->findAll();
+      return $this->render('sil16VitrineBundle:ProductCategory:index.html.twig', array('product_categories' => $product_categories));
     }
 }
